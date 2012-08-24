ea7ee661b7bfa686b9c41d96c0971cf4ce4f773d
Steffen Schwigon <schwigon@cpan.org>
2012-08-24 13:25:16 +0000

    Import of SCHWIGON/App-Bootstrap-Perl-0.03 from CPAN.
    gitpan-cpan-distribution: App-Bootstrap-Perl
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SCHWIGON/App-Bootstrap-Perl-0.03.tar.gz
    gitpan-cpan-author:       SCHWIGON
    gitpan-cpan-maturity:     released

--- lib/App/Bootstrap/Perl.pm
+++ lib/App/Bootstrap/Perl.pm
@@ -1,72 +1,45 @@
 package App::Bootstrap::Perl;
+# git description: a106258
 
-use strict;
-use warnings;
-
-our $VERSION = '0.02';
+BEGIN {
+  $App::Bootstrap::Perl::AUTHORITY = 'cpan:SCHWIGON';
+}
+{
+  $App::Bootstrap::Perl::VERSION = '0.03';
+}
+# ABSTRACT: Bootstrap Perl inclusive CPAN from git
 
 1;
 
-__END__
+
+
+=pod
+
+=encoding utf-8
 
 =head1 NAME
 
-App::Bootstrap:Perl - Bootstrap Perl inclusive CPAN from git
+App::Bootstrap::Perl - Bootstrap Perl inclusive CPAN from git
 
 =head1 ABOUT
 
-This is the corresponding namespace for B<bootstrap-perl>, a tool to
-bootstrap Perl installations inclusive a CPAN environment from git.
-
-B<bootstrap-perl> can compile and setup a complete Perl environment
-from git, inclusive initial CPAN setup with distroprefs to ensure
-completely unattended installation.
-
-See L<bootstrap-perl|bootstrap-perl> for details.
+L<bootstrap-perl|bootstrap-perl> can compile and setup a complete Perl
+environment from git, inclusive initial CPAN setup with distroprefs to
+ensure completely unattended installation.
 
 =head1 AUTHOR
 
-Steffen Schwigon, C<< <ss5 at renormalist.net> >>
+Steffen Schwigon <ss5@renormalist.net>
 
-=head1 BUGS
+=head1 COPYRIGHT AND LICENSE
 
-Please report any bugs or feature requests to C<bug-app-bootstrap-perl at
-rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Bootstrap-Perl>.  I will
-be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
+This software is copyright (c) 2012 by Steffen Schwigon.
 
-
-=head1 SUPPORT
-
-You can look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Bootstrap-Perl>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/App-Bootstrap-Perl>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/App-Bootstrap-Perl>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/App-Bootstrap-Perl/>
-
-=back
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2011 Steffen Schwigon, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
+
+
+__END__
+
